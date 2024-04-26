3ec198a38f241398c8c6616410c492c874f79883
Tong Zhang <zhangt@frib.msu.edu>
2024-04-26 09:56:59 -0400

    REF: Build a qt6 app, updated the input arguments.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 #
-echo "Hello $1"
+
+PROJ_NAME=$1
+APP_NAME=$2
+
+echo "Build Qt project: ${PROJ_NAME} to ${APP_NAME}"
 echo "time=$(date)" >> $GITHUB_OUTPUT
 
-pwd
-ls -lhtr
+# build
+qmake CONFIG+=release CONFIG+=optimize_full ${PROJ_NAME}
+make -j$(cat /proc/cpuinfo | /bin/grep processor | wc -l)
