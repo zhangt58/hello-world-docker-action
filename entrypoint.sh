2a88ca32886873371bff74699abfcadbb7eb0b27
Tong Zhang <zhangt@frib.msu.edu>
2024-04-26 10:05:38 -0400

    REF: Test dist_dir argument, defaults to 'dist'.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@
 
 PROJ_NAME=$1
 APP_NAME=$2
+DIST_DIR=$3
 
-echo "Build Qt project: ${PROJ_NAME} to ${APP_NAME}"
+echo "Build Qt project: ${PROJ_NAME} as ${APP_NAME}, keep artifacts at ${DIST_DIR} "
 echo "time=$(date)" >> $GITHUB_OUTPUT
 
 # build
